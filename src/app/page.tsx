bce419150dcf015c091a49a8705eddf3dbea1150
theUniParse <theUniParse@gmail.com>
2023-09-24 22:05:23 +0100

    typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
     <main className='text-center'>
       <h1>Home</h1>
-      <p>profie: {session?.user?.name || 'Not signed in'}</p>
+      <p>profile: {session?.user?.name || 'Not signed in'}</p>
     </main>
   )
 }
