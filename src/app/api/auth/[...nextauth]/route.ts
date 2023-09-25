2b35d8f88824ae6e7b268d932d3a3ddaf488e123
theUniParse <theUniParse@gmail.com>
2023-09-25 08:29:33 +0100

    add the providers google, facebook and twetter

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,29 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import FacebookProvider from 'next-auth/providers/facebook'
 import GithubProvider from 'next-auth/providers/github'
+import TwitterProvider from 'next-auth/providers/twitter'
 
-const githubProvider = GithubProvider({
-  clientId: process.env.GITHUB_ID ?? '',
-  clientSecret: process.env.GITHUB_SECRET ?? '',
-})
+const providers = [
+  GoogleProvider({
+    clientId: process.env.GOOGLE_ID as string,
+    clientSecret: process.env.GOOGLE_SECRET as string,
+  }),
+  FacebookProvider({
+    clientId: process.env.FACEBOOK_ID as string,
+    clientSecret: process.env.FACEBOOK_SECRET as string,
+  }),
+  TwitterProvider({
+    clientId: process.env.TWITTER_ID as string,
+    clientSecret: process.env.TWITTER_SECRET as string,
+  }),
+  GithubProvider({
+    clientId: process.env.GITHUB_ID as string,
+    clientSecret: process.env.GITHUB_SECRET as string,
+  }),
+]
 
-export const authOptions = {
-  providers: [githubProvider],
-}
+export const authOptions: NextAuthOptions = { providers }
 
 export const handler = NextAuth(authOptions)
 
